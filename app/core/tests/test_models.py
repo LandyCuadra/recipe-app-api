6af9edaefe23cdb25817b626dab254e7b57501a6
Orlando Cuadra <orlandocuadra@Orlando-NS.local>
2025-01-10 10:56:51 -0600

    add tags and partial ingredients api

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,10 +2,16 @@
 Test for models.
 """
 
+from contextlib import closing
 from decimal import Decimal
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from core import models
+
+
+def create_user(email='user@example.com', password='testpass123'):
+    """Creates and return a new user"""
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -64,3 +70,19 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag successful"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingredient(self):
+        """Test creating an ingredient is successful"""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Ingredient1'
+        )
+        self.assertEqual(str(ingredient), ingredient.name)
